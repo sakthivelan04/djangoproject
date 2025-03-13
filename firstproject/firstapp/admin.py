5f9290e60905fb846813093c2597751a31e6c6bb
sakthivelan04 <149371985+sakthivelan04@users.noreply.github.com>
2025-03-12 22:45:18 -0700

    Delete firstproject directory

--- firstproject/firstapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from firstapp.models import cultural
-
-# Register your models here.
-
-class CulturalAdmin(admin.ModelAdmin):
-    list_display = ('Clg_Name','Clg_Address','Event_Name','Date','cultural_Poster')
-admin.site.register(cultural,CulturalAdmin)
--- firstproject/secondapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Student
-
-class StudentAdmin(admin.ModelAdmin):
-    list_display = ('id', 'username', 'register_no', 'email', 'collage')
-
-admin.site.register(Student, StudentAdmin)
